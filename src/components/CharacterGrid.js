06720bf1f63ae076880929caa1033bc79489d0b7
raavinduf <ravinduf@rocketmail.com>
2020-07-10 22:09:16 +0530

    fixed key error

--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CharacterGrid = ({items, isLoading }) => {
     return isLoading ? <h1>Loading...</h1> : <section className="cards">
-        {items.map(item => (<h1>{item.name}</h1>))}
+        {items.map(item => (<h1 key={item.char_id}>{item.name}</h1>))}
     </section>
 }
 
