85b0cb69a69d6c4954d8573366371ba58835784d
raavinduf <ravinduf@rocketmail.com>
2020-07-10 14:10:07 +0530

    created state and obtained data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 import Header from './components/Header';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      const fetchItems = async () => {
+          const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+
+          console.log(result.data);
+      }
+
+      fetchItems();
+  }, [])
+
   return(
     <div className='container'>
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../img/logo.png';
 
 const Header = () => {
+
     return (
         <header className="center">
             <img src={logo} alt="" />
