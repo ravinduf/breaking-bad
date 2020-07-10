8fe955bb16bc071e83bf946fdd943c4302c76225
raavinduf <ravinduf@rocketmail.com>
2020-07-10 22:57:57 +0530

    added state and onchange function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 
 const Search = () => {
+    const [text, setText] = useState('');
+
     return (
         <section className='search'>
             <input type="text" 
                 className='form-control' 
-                placeholder="Search characters" 
+                placeholder="Search characters"
+                value={text} 
+                onChange={(e) => setText(e.target.value)}
                 autoFocus 
             />
+            
         </section>
     )
 }
